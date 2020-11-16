479d3a1555bdd8e10fa192a17e0087d59c384fd6
Laranginha <info@softdiscover.com>
2020-11-15 23:01:38 -0500

    creating api controller php artisan make:controller ThoughtController --api

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,4 @@ Auth::routes(['register' => true]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-
+Route::apiResource('thoughts', 'ThoughtController');
